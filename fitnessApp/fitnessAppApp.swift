c009793916367951012467c392abe3c79582f81a
Sam Gusick <sgusick@uvm.edu>
2022-10-18 15:29:52 -0400

    Initial commit

---
+++ fitnessApp/fitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitnessAppApp.swift
+//  fitnessApp
+//
+//  Created by Sam Gusick on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct fitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
