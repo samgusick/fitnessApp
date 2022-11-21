a7760eaf858db78f43ce09b0089a837a13eccb84
marajadeskywalker <maxwell.heath@uvm.edu>
2022-11-21 16:41:05 -0500

    created settings tab

--- fitnessApp/Views/SettingsView.swift
+++ fitnessApp/Views/SettingsView.swift
@@ -4,28 +4,57 @@
 //
 //  Created by Liam Hackett on 10/24/22.
 //
-
+import Foundation
 import SwiftUI
 
 struct SettingsView: View {
     @State var str : String = ""
+    @ObservedObject var settings: Settings = Settings()
     var body: some View {
         
         NavigationView {
             VStack{
-                TextField("Username: ", text: $str).background(Rectangle().fill(.white))
-            }
-            .navigationBarTitle(Text("Settings"))
-            .navigationBarItems(leading: Image(systemName: "gear").foregroundColor(.black))//systemName: "figure.run")) not available ios 15
-            .font(.title)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color(red: 0.4, green: 0.6, blue: 0.8).edgesIgnoringSafeArea(.all))
+                TextField("Username: ", text: $str)
+                    .frame(width:300)
+                    .background(Rectangle().fill(settings.accentColor))
+                Menu {
+                    Button("Blue", action: {
+                        settings.changeColor(accent: Color(red: 0.671, green: 0.78, blue: 0.9), background: Color(red: 0.4, green: 0.6, blue: 0.8))
+                        
+                    });
+                    Button("Pink", action: {
+                        settings.changeColor(accent: Color(red: 0.9, green: 0.671, blue: 0.78), background: Color(red: 0.8, green: 0.4, blue: 0.6))
+                        
+                    })
+                    Button("Green", action: {
+                        settings.changeColor(accent: Color(red: 0.671, green: 0.9, blue: 0.78), background: Color(red: 0.4, green: 0.8, blue: 0.6))
+                        
+                    })
+                } label: {
+                    Text("Background Color")
+                    .foregroundColor(.black)
+                    .frame(width: 300, height: 100)
+                    .background(Rectangle().fill(settings.accentColor)
+                        .cornerRadius(10))
+                }
+                
+                /*
+                .navigationBarTitle(Text("Settings"))
+                .navigationBarItems(leading: Image(systemName: "gear").foregroundColor(.black))//systemName: "figure.run")) not available ios 15
+                .font(.title)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Rectangle().fill(settings.backgroundColor)).edgesIgnoringSafeArea(.all)*/
+            }.navigationBarTitle(Text("Settings"))
+                .navigationBarItems(leading: Image(systemName: "gear").foregroundColor(.black))//systemName: "figure.run")) not available ios 15
+                .font(.title)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Rectangle().fill(settings.backgroundColor).edgesIgnoringSafeArea(.all))
         }
     }
-}
-
-struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView()
+    
+    struct SettingsView_Previews: PreviewProvider {
+        static var previews: some View {
+            SettingsView()
+        }
     }
 }
