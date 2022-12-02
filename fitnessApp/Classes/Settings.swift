1aaa4e6d259b2b6f6047d89dcf1f455999fe8a25
marajadeskywalker <maxwell.heath@uvm.edu>
2022-12-02 17:13:00 -0500

    added settings controls for workout scheduling g

--- fitnessApp/Classes/Settings.swift
+++ fitnessApp/Classes/Settings.swift
@@ -18,13 +18,13 @@ class Settings : ObservableObject {
     @Published var backgroundColor = Color(red: 0.4, green: 0.6, blue: 0.8)
     @Published var accentColor = Color(red: 0.671, green: 0.78, blue: 0.9)
 
-    @Published var sunday : String = "Off Day"
-    @Published var monday : String = "Push Day"
-    @Published var tuesday : String = "Pull Day"
-    @Published var wednesday : String = "Leg Day"
-    @Published var thursday : String = "Push Day"
-    @Published var friday : String = "Pull Day"
-    @Published var saturday : String = "Off Day"
+    @Published var sunday : days = .off
+    @Published var monday : days = .push
+    @Published var tuesday : days = .pull
+    @Published var wednesday : days = .leg
+    @Published var thursday : days = .push
+    @Published var friday : days = .pull
+    @Published var saturday : days = .off
     
     @Published var goal : CGFloat = 5000
     
@@ -37,6 +37,9 @@ class Settings : ObservableObject {
     func changeColor(accent: Color, background: Color) {
         changeBackgroundColor(background)
         changeAccentColor(accent)
+    }
+    func changeDay(day: inout days, workout: days) {
+        day = workout
     }
 }
 
